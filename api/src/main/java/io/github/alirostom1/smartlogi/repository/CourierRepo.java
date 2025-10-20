fdd36efb42b4d7416f763aef88a5110004bd8bcb
ali-rostom1 <ali.rostom220@gmail.com>
2025-10-20 14:45:46 +0100

    feat: SL-9 add find by phone method in courier repository https://alirostom2201.atlassian.net/jira/software/projects/LS/boards/233?selectedIssue=LS-9

--- api/src/main/java/io/github/alirostom1/smartlogi/repository/CourierRepo.java
+++ api/src/main/java/io/github/alirostom1/smartlogi/repository/CourierRepo.java
@@ -3,7 +3,9 @@ package io.github.alirostom1.smartlogi.repository;
 import io.github.alirostom1.smartlogi.model.entity.Courier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CourierRepo extends JpaRepository<Courier, UUID> {
+    public Optional<Courier> findByPhoneNumber();
 }
