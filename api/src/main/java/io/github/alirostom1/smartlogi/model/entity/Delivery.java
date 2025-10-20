1f1d73389b4295a9581d17c47416be2e4a1a3b7f
ali-rostom1 <ali.rostom220@gmail.com>
2025-10-20 14:12:21 +0100

    feat: SL-6 add delivery entity https://alirostom2201.atlassian.net/jira/software/projects/LS/boards/233?selectedIssue=LS-6

---
+++ api/src/main/java/io/github/alirostom1/smartlogi/model/entity/Delivery.java
@@ -0,0 +1,49 @@
+package io.github.alirostom1.smartlogi.model.entity;
+
+
+import io.github.alirostom1.smartlogi.model.enums.DeliveryStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String recipient;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status = DeliveryStatus.PREPARATION;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+    public Delivery(String recipient, double weight, String address, DeliveryStatus status) {
+        this.recipient = recipient;
+        this.weight = weight;
+        this.address = address;
+        this.status = status;
+    }
+}
