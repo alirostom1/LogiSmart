c236d1649ddbc5c7341cfc2fcf57eb986007b36b
ali-rostom1 <ali.rostom220@gmail.com>
2025-10-24 11:01:50 +0100

    fix: fix repository interfaces package names

--- api/src/main/java/io/github/alirostom1/logismart/repository/DeliveryRepo.java
+++ api/src/main/java/io/github/alirostom1/logismart/repository/DeliveryRepo.java
@@ -1,7 +1,7 @@
-package io.github.alirostom1.smartlogi.repository;
+package io.github.alirostom1.logismart.repository;
 
 
-import io.github.alirostom1.smartlogi.model.entity.Delivery;
+import io.github.alirostom1.logismart.model.entity.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
