978a3ab0f024bf977f3e8234e91ea6151b4335a7
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-02 20:36:45 +0100

    feat: LS-4 add repository methods to be implemented in service later https://alirostom2201.atlassian.net/jira/software/projects/LS/boards/233?selectedIssue=LS-4

--- api/src/main/java/io/github/alirostom1/logismart/repository/ProductRepo.java
+++ api/src/main/java/io/github/alirostom1/logismart/repository/ProductRepo.java
@@ -1,9 +1,16 @@
 package io.github.alirostom1.logismart.repository;
 
 import io.github.alirostom1.logismart.model.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ProductRepo extends JpaRepository<Product, UUID> {
+    boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name,UUID id);
+
+    Page<Product> findByNameContainingIgnoreCaseOrCategoryContainingIgnoreCase(String name, String category, Pageable pageable);
+    Page<Product> findByCategoryContainingIgnoreCase(String category,Pageable pageable);
 }
