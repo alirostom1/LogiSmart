3a2486ecc700853c6046e4b1aa3dd5ea0b3973f3
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-03 16:01:26 +0100

    feat: add ressource not found exception handler thrown from service methods

--- api/src/main/java/io/github/alirostom1/logismart/advice/GlobalExceptionHandler.java
+++ api/src/main/java/io/github/alirostom1/logismart/advice/GlobalExceptionHandler.java
@@ -4,13 +4,12 @@ package io.github.alirostom1.logismart.advice;
 import io.github.alirostom1.logismart.dto.response.common.ApiResponse;
 
 import io.github.alirostom1.logismart.exception.PostalCodeAlreadyExistsException;
+import io.github.alirostom1.logismart.exception.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
-import org.springframework.web.servlet.View;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +32,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(apiResponse);
     }
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<ApiResponse<List<String>>> handleHandlerMethodValidationException(HandlerMethodValidationException ex){
+    public ResponseEntity<ApiResponse<List<String>>> handleHandlerMethodValidation(HandlerMethodValidationException ex){
         List<String> errors = new ArrayList<>();
         ex.getParameterValidationResults().forEach(validationResult ->{
             String parameterName = validationResult.getMethodParameter().getParameterName();
@@ -53,8 +52,18 @@ public class GlobalExceptionHandler {
 
     //CUSTOM EXCEPTIONS
     @ExceptionHandler(PostalCodeAlreadyExistsException.class)
-    public ResponseEntity<ApiResponse<?>> handlePostalCodeDuplication(PostalCodeAlreadyExistsException ex){
-        ApiResponse<?> apiResponse = new ApiResponse<>(
+    public ResponseEntity<ApiResponse<Void>> handlePostalCodeDuplication(PostalCodeAlreadyExistsException ex){
+        ApiResponse<Void> apiResponse = new ApiResponse<>(
+                false,
+                ex.getMessage(),
+                null,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFound(ResourceNotFoundException ex){
+        ApiResponse<Void> apiResponse = new ApiResponse<>(
                 false,
                 ex.getMessage(),
                 null,
