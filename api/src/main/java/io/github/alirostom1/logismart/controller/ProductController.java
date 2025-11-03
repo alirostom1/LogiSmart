b09737ce714049d84aa5959413a5481c9dcd545a
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-03 15:59:13 +0100

    feat: LS-31 add product controller with basic crud functionalities https://alirostom2201.atlassian.net/jira/software/projects/LS/boards/233?selectedIssue=LS-31

---
+++ api/src/main/java/io/github/alirostom1/logismart/controller/ProductController.java
@@ -0,0 +1,84 @@
+package io.github.alirostom1.logismart.controller;
+
+import io.github.alirostom1.logismart.dto.request.product.CreateProductRequest;
+import io.github.alirostom1.logismart.dto.response.common.ApiResponse;
+import io.github.alirostom1.logismart.dto.response.product.ProductResponse;
+import io.github.alirostom1.logismart.service.ProductService;
+import io.github.alirostom1.logismart.util.ValidUUID;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v2/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<ProductResponse>>> index(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size){
+        Pageable pageable = PageRequest.of(page,size);
+        Page<ProductResponse> productsPage = productService.getAllProducts(pageable);
+        ApiResponse<Page<ProductResponse>> apiResponse = new ApiResponse<>(
+                true,
+                "Products retrieved successfully",
+                productsPage,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.ok(apiResponse);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProductResponse>> show(@PathVariable("id") @ValidUUID String id){
+        ProductResponse productResponse = productService.getProductById(id);
+        ApiResponse<ProductResponse> apiResponse = new ApiResponse<>(
+                true,
+                "Product Retrieved successfully",
+                productResponse,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.ok(apiResponse);
+    }
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductResponse>> store(@Valid @RequestBody CreateProductRequest request){
+        ProductResponse productResponse = productService.createProduct(request);
+        ApiResponse<ProductResponse> apiResponse = new ApiResponse<>(
+                true,
+                "Product created successfully!",
+                productResponse,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<ProductResponse>> update(
+                @PathVariable("id") @ValidUUID String id,
+                @Valid @RequestBody CreateProductRequest request){
+        ProductResponse productResponse = productService.updateProduct(id,request);
+        ApiResponse<ProductResponse> apiResponse = new ApiResponse<>(
+                true,
+                "Product updated successfully!",
+                productResponse,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable("id") @ValidUUID String id){
+        productService.deleteProduct(id);
+        ApiResponse<Void> apiResponse = new ApiResponse<>(
+                true,
+                "Product deleted successfully!",
+                null,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.ok(apiResponse);
+    }
+
+}
