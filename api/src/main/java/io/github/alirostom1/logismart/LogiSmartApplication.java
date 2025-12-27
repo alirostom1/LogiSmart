635ea7ef8716ce480d035603bbaa1c92ff27b9c2
ali-rostom1 <ali.rostom220@gmail.com>
2025-12-27 13:31:22 +0100

    feat: add Oauth 2 success handler class to properly get or save user and return tokens correspondingly

--- api/src/main/java/io/github/alirostom1/logismart/LogiSmartApplication.java
+++ api/src/main/java/io/github/alirostom1/logismart/LogiSmartApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.util.UUID;
-
 @SpringBootApplication
 @EnableWebMvc
 @EnableJpaAuditing
