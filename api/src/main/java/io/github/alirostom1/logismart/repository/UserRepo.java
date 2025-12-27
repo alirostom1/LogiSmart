014b73327c32fda7d7eb75bfc5239340f1034bc9
ali-rostom1 <ali.rostom220@gmail.com>
2025-12-27 13:28:24 +0100

    feat: modify user model and repo for oauth2 provider and provider id checks

--- api/src/main/java/io/github/alirostom1/logismart/repository/UserRepo.java
+++ api/src/main/java/io/github/alirostom1/logismart/repository/UserRepo.java
@@ -1,6 +1,7 @@
 package io.github.alirostom1.logismart.repository;
 
 import io.github.alirostom1.logismart.model.entity.*;
+import io.github.alirostom1.logismart.model.enums.AuthProvider;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,6 @@ import java.util.UUID;
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider,String providerId);
 }
